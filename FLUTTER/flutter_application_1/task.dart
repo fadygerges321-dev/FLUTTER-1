5fa1e198df7cb7230489baccdd89a6c2dd5ec82e
fady gerges <fadygerges321@icloud.com>
2026-09-05 16:46:58 +0300

    Add files via upload

---
+++ FLUTTER/flutter_application_1/task.dart
@@ -0,0 +1,20 @@
+void main() {
+  int calclulate (int num1, int num2, string operation) {
+    switch (operation) {
+      case " +":
+       return num1 +num2;
+        case " -":
+        return num1 - num2;
+        case " *":
+        return num1 * num2;
+        case "/":
+        return num 1 /num2;
+default:
+return 0;
+
+
+        
+    }
+  }
+   print calclulate(10, 5, "+");
+}
